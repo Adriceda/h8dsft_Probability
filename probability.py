21ca0582aa4833ee8577bf94c8b2d78874f38374
Adriceda Irsia <adriceda.irsia@gmail.com>
2023-02-07 16:28:48 +0700

    probability.py

--- probability.py
+++ probability.py
@@ -7,21 +7,27 @@ import random
 # # Count the number of 3xHeads results
 # h3 = 0
 
+
 # Create a list of all results
 results = []
 
 # loop through 10000 trials
-trials = 10000
+trials = 50000
 trial = 0
+heads = 0
+tails = 0
+
 while trial < trials:
     trial = trial + 1
-    # Flip three coins
-    result = 'H' if random.randint(0,1) == 0 else 'T'
+    result = random.randint(0,1)
+    if result == 1: 
+        heads += 1
+    else:
+        tails += 1
     results.append(result)
-    # If it's three heads, add it to the count
-    h3 = h3 + int(result == ['H','H','H'])
     
-# What proportion of trials produced 3x heads
-print ("%.2f%%" % ((h3/trials)*100))
 
-print (results)
+# print (results)
+print('TRIAL = ', trial)
+print('HEADS = ', heads)
+print('TAILS = ', tails)
